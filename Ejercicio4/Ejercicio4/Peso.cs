079ee5c9f577b689704060e55ee7e072285918ed
Jon Carrasco <jcarrascobar@gmail.com>
2018-10-15 17:58:29 +0200

    ej4

---
+++ Ejercicio4/Ejercicio4/Peso.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4
+{
+    class Peso
+    { 
+
+        private double kg;
+        private String medida;
+
+        public Peso()
+        {
+
+        }
+
+        public Peso(double peso, string tipo)
+        {
+            this.kg = GetKilos(peso, tipo);
+        }
+
+        public double GetKg()
+        {
+        return kg;
+        }
+
+       public void SetKg(double kg)
+        {
+        this.kg = kg;
+        }
+
+        public String GetMedida()
+        {
+        return medida;
+        }
+
+        public void SetMedida(string medida)
+        {
+        this.medida = medida;
+        }
+
+        public double GetLibras()
+        {
+        return GetKg() / 0.453;
+        }
+
+        public double GetLingotes()
+        {
+        return GetKg() / 14.59;
+        }
+
+        public double GetKilos(double peso, String tipo)
+        {
+            switch (tipo.ToUpper())
+            {
+            case "LB":
+                return peso * 0.453;
+            case "LI":
+                return peso * 14.59;
+            case "OZ":
+                return peso * 0.2835;
+            case "P":
+                return peso * 0.00155;
+            case "K":
+                return peso;
+            case "G":
+                return peso * 1000;
+            case "Q":
+                return peso * 43.3;
+            default:
+                Console.WriteLine("La medida introducida es incorrecta");
+                Console.ReadLine();
+                return peso;
+            }
+        }
+
+    public double GetPeso(string tipo)
+    {
+        switch (tipo.ToUpper())
+        {
+            case "LB":
+                return GetLibras();
+            case "LI":
+                return GetLingotes();
+            case "OZ":
+                return GetKg() / 0.2835;
+            case "P":
+                return GetKg() / 0.00155;
+            case "K":
+                return GetKg();
+            case "G":
+                return GetKg() * 1000;
+            case "Q":
+                return GetKg() / 43.3;
+            default:
+                Console.WriteLine("La medida introducida es incorrecta");
+                Console.ReadLine();
+                return kg;
+            }
+        }
+
+
+    }
+}
---
+++ Ejercicio4/Ejercicio4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Deberá tener un atributo donde se almacene el peso de un objeto en kilogramos.
+            //En el constructor se le pasará el peso y la medida en la que se ha tomado(‘Lb’ para libras, ‘Li’ para lingotes,
+            //‘Oz’ para onzas, ‘P’ para peniques, ‘K’ para kilos, ‘G’ para gramos y ‘Q’ para quintales).
+            //Deberá de tener los siguientes métodos:
+            //GetLibras.Devuelve el peso en libras.
+            //GetLingotes.Devuelve el peso en lingotes.
+            //GetPeso.Devuelve el peso en la medida que se pase como parámetro(‘Lb’ para libras, ‘Li’ para lingotes,
+            //‘Oz’ para onzas, ‘P’ para peniques, ‘K’ para kilos, ‘G’ para gramos y ‘Q’ para quintales).
+            //Para la realización del ejercicio toma como referencia los siguientes datos:
+            //1 Libra = 16 onzas = 453 gramos.
+            //1 Lingote = 32,17 libras = 14,59 kg.
+            //1 Onza = 0,0625 libras = 28,35 gramos.
+            //1 Penique = 0,05 onzas = 1,55 gramos.
+            //1 Quintal = 100 libras = 43,3 kg.
+            //Crea además un método main para testear y verificar los métodos de esta clase
+
+            int peso = 5;
+            String tipo = "LB";
+            Peso p1 = new Peso(peso, tipo);
+            //Console.WriteLine(p1.GetPeso(p1.GetKg(), "lb"));
+            Console.WriteLine(peso + " " + tipo + " = " + p1.GetKg() + "kg");
+            Console.WriteLine(p1.GetKg() + "kg =" + p1.GetPeso("lb") + " libras");
+            Console.ReadLine();
+        }
+        
+    }
+}
