88ea0069dc1c132c8afd435c3bf121417aa5c559
Jon Carrasco <jcarrascobar@gmail.com>
2018-10-15 18:47:38 +0200

    Ej6

---
+++ Ejercicio 6/Ejercicio 6/Coche.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_6
+{
+    class Coche
+    {
+        private string marca;
+        private string modelo;
+
+        public Coche()
+        {
+
+        }
+        public Coche(string marca, string modelo)
+        {
+            this.marca = marca;
+            this.modelo = modelo;
+        }
+
+        public string Getmarca()
+        {
+            return marca;
+        }
+        public void Setmarca(string marca)
+        {
+            this.marca = marca;
+        }
+
+        public string Getmodelo()
+        {
+            return modelo;
+        }
+        public void Setmodelo(string modelo)
+        {
+            this.modelo = modelo;
+        }
+
+
+    }
+}
---
+++ Ejercicio 6/Ejercicio 6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crea la clase coche con dos constructores. Uno no toma parámetros y el otro sí. Los dos constructores inicializarán 
+            //los atributos marca y modelo de la clase.
+            //Implementa métodos para obtener y establecer el valor de los atributos, así como un método para mostrar toda la información.
+            //Crea dos objetos(cada objeto llama a un constructor distinto) y verifica que todo funciona correctamente.
+
+
+        }
+    }
+}
