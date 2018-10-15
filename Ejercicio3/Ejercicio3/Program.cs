f2d3966132d27913186c5163d83f3547e107ab0e
Jon Carrasco <jcarrascobar@gmail.com>
2018-10-15 16:38:55 +0200

    Ej3

---
+++ Ejercicio3/Ejercicio3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Modifica la clase satélite y añádele los siguientes métodos:
+            //Método void VariaAltura(double desplazamiento): Este método acepta un parámetro que será positivo o negativo 
+            //dependiendo de si el satélite tiene que alejarse o acercarse a La Tierra.
+            //Método boolean EnOrbita(): Este método devolverá false si el satélite está en tierra, y true en caso contrario.
+            //Método void VariaPosicion(double variap, double variam): Este método permite modificar los atributos de posición
+            //(meridiano y paralelo) mediante los parámetros variap y variam. Estos parámetros serán valores positivos 
+            //o negativos relativos que harán al satélite modificar su posición.
+
+            Satelite sat1 = new Satelite();
+            sat1.SetPosicion(66, 23, 36000);
+
+            
+            sat1.VariarAltura(-560);
+            Console.WriteLine(sat1.EnOrbita());
+            sat1.VariaPosicion(20,-36);
+            sat1.PrintPosicion();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Ejercicio3/Ejercicio3/Satelite.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    
+        public class Satelite
+        {
+            private double meridiano;
+            private double paralelo;
+            private double distancia_tierra;
+
+            public Satelite(double m, double p, double d)
+            {
+                meridiano = m;
+                paralelo = p;
+                distancia_tierra = d;
+            }
+
+            public double Getmeridiano()
+            {
+                return meridiano;
+            }
+            public void Setmeridiano(double meridiano)
+            {
+                this.meridiano = meridiano;
+            }
+            public double Getparalelo()
+            {
+                return paralelo;
+            }
+            public void Setparalelo(double paralelo)
+            {
+                this.paralelo = paralelo;
+            }
+            public double Getdistancia_tierra()
+            {
+                return distancia_tierra;
+            }
+            public void Setdistancia_tierra(double distancia_tierra)
+            {
+                this.distancia_tierra = distancia_tierra;
+            }
+
+
+            public Satelite()
+            {
+                meridiano = paralelo = distancia_tierra = 0;
+            }
+
+            public void SetPosicion(double m, double p, double d)
+            {
+                meridiano = m;
+                paralelo = p;
+                distancia_tierra = d;
+            }
+
+            public void PrintPosicion()
+            {
+                Console.WriteLine("El satélite se encuentra en el paralelo " + paralelo + " Meridiano "+meridiano+ " a una distancia de la tierra de "+distancia_tierra+"Kilómetros");
+                Console.ReadLine();
+            }
+
+            public  void VariarAltura(double desplazamiento)
+            {
+                if (desplazamiento < 0)
+                {
+                    Console.WriteLine("se esta acercando a la tierra");
+                }
+                else if (desplazamiento > 0)
+                {
+                    Console.WriteLine("se aleja de la Tierra");
+                }
+                else
+                {
+                    Console.WriteLine("no se mueve");
+                }
+            }
+            public bool EnOrbita()
+            {
+                if (distancia_tierra == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+                
+            }
+            
+            public void VariaPosicion (double variap, double variam)
+            {
+                meridiano = meridiano + variam;
+                paralelo = paralelo + variap;
+            }
+
+        }
+}
